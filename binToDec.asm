5d57d1e4d9fd4836616f11c08f1f60d534bbbafb
Bryan Stetson <bryanstetson@gmail.com>
2014-05-04 00:59:04 -0400

    -Finished the god awful binary to octal conversions -Created a UI (so to speak) so that the user can now select which conversion they would like (instead of having it spit all of them out). This was an added spec by the prof. -Made it so that the program loops upon successful completion of a conversion. (currently there is no way to exit outright, but fuck if I'm gunna start messing around with user inputing strings and checking 1s and 0s and exits...)

--- binToDec.asm
+++ binToDec.asm
@@ -1,32 +1,71 @@
+#Go TEAM DOGE
+#Infinite amount of help:
+#49 is ASCII code for '1'
+#48 is ASCII code for '0'
+
 	.data
-prompt:	.asciiz "Enter a binary number: "
-empty:	.space 16
-result:	.asciiz "\nDecimal: "
+initPrompt: .asciiz "Welcome to the binary converter!"
+binPrompt:	.asciiz "\nEnter a binary number: "
+basePrompt:	.asciiz "Enter a base (10=Decimal, 8=Octal, 16=Hex): "
+
+test:	.asciiz "test"
+
+binNum:		.space 16
+decResult:	.asciiz "Decimal: "
+octResult:	.asciiz "Octal: "
+#hexResult:	.asciiz "Hex: "			#UNCOMMENT!!
 	
 	.text
 	.globl main
 main:
-	# Print prompt to user
+	# Print initial welcome prompt to user
 	li	$v0, 4        
-	la 	$a0, prompt    
+	la 	$a0, initPrompt    
 	syscall
 
-	# Get input from user
-	la	$a0, empty
+BinaryPrompt:
+	# Print binary prompt to user
+	li	$v0, 4        
+	la 	$a0, binPrompt    
+	syscall
+
+	# Get binary input from user
+	la	$a0, binNum
 	li	$a1, 16		
 	li	$v0, 8          
 	syscall
 
+	#Insert some binary number error checks
+
+	# Stuff that needs to get reset every run 
 	# Initialize result to zero
 	li	$t4, 0               
+	# Address of binary number in $t1
+	la	$t1, binNum
+	# Initialize binToDec counter to 16
+	li	$t9, 16   
+	# Initialize binToOct counter to 0
+	li	$t8, 0
 
-	la	$t1, empty
-	# Initialize counter to 16
-	li	$t9, 16           
+BasePrompt:
+	# Print base conversion prompt to user
+	li	$v0, 4        
+	la 	$a0, basePrompt    
+	syscall
+
+	# Get conversion base input from user
+	li	$v0, 5		#5 is sys read int          
+	syscall
+
+	# Check that the base is valid
+	beq $v0, 10, binToDec	# Jump to decimal convert if 10
+	beq $v0, 8, binToOct	# Jump to octal convert if 8
+	#beq $v0, 16, binToHex	# Jump to hex convert if 16  		#UNCOMMENT!!
+	j	BasePrompt
 
 binToDec:
 	lb	$a0, ($t1)
-	blt	$a0, 48, printResult
+	blt	$a0, 48, printDecResult
 	# Increment offset
 	addi	$t1, $t1, 1
 	# Convert to decimal
@@ -35,7 +74,7 @@ binToDec:
 	addi	$t9, $t9, -1
 	beq	$a0, 0, zero
 	beq	$a0, 1, one
-	j	printResult
+	j	printDecResult
 
 zero:
 	j	binToDec
@@ -49,11 +88,11 @@ one:
 
 	j 	binToDec
 
-printResult:
+printDecResult:
 	srlv 	$t4, $t4, $t9
 
 	# Print result header
-	la 	$a0, result
+	la 	$a0, decResult
 	li 	$v0, 4
 	syscall
 
@@ -61,9 +100,75 @@ printResult:
 	move	$a0, $t4    
 	li 	$v0, 1      
 	syscall
+	j 	exit
+
+binToOct:
+	# Counter1 = $t7, keeps track of current bit
+	# Counter2 = $t8, keeps track of current octal
+	# Answer = $t4
+	# Temp = t3, temporarily holds values to be added to the answer
+	# Extra = t5, used for calculations
+	# Reset counter for current bit
+	li 	$t3, 0
+	li	$t7, 0
+
+	lb	$a0, ($t1)					# Take first input bit
+	blt	$a0, 48, printOctResult 	# Go to the end
+	addi	$t1, $t1, 1 			# Increment offset
+	addi	$a0, $a0, -48			# Convert to 1 or 0
+	sllv	$a0, $a0, $t7			# Shift into position
+	addi	$t7, $t7, 1 			# Increment count1
+	or 		$t3, $t3, $a0			# Place bit onto temp answer
+
+	lb	$a0, ($t1)					# Take second input bit
+	blt	$a0, 48, octProcess 		# Go to the octal processing
+	addi	$t1, $t1, 1 			# Increment offset
+	addi	$a0, $a0, -48			# Convert to 1 or 0
+	sllv	$a0, $a0, $t7			# Shift into position
+	addi	$t7, $t7, 1 			# Increment count1
+	or 		$t3, $t3, $a0			# Place bit onto temp answer
+
+	lb	$a0, ($t1)					# Take third input bit
+	blt	$a0, 48, octProcess 		# Go to the octal processing
+	addi	$t1, $t1, 1 			# Increment offset
+	addi	$a0, $a0, -48			# Convert to 1 or 0
+	sllv	$a0, $a0, $t7			# Shift into position
+	addi	$t7, $t7, 1 			# Increment count1
+	or 		$t3, $t3, $a0			# Place bit onto temp answer
+
+octProcess:
+	beq		$t8, 0, octContinue
+	add 	$t2, $zero, $t8			# Copy $t8 to $t2
+
+octPower:							# Basically does $t3*10^$t8
+	beq		$t2, 0, octContinue		# $t2 is a countdown
+	li		$t5, 10				
+	mult	$t3, $t5				
+	mflo 	$t3
+	addi	$t2, $t2, -1 			# decrement count
+	j 		octPower 				
+
+octContinue:
+	add 	$t4, $t4, $t3
+	addi	$t8, $t8, 1 			# Increment count2
+	j 		binToOct
+
+
+printOctResult:
+	# Print result header
+	la 	$a0, octResult
+	li 	$v0, 4
+	syscall
+
+	# Get converted number and print
+	move	$a0, $t4    
+	li 	$v0, 1      
+	syscall
+	j 	exit
+
 
 exit:
-	# Exit the program
-	li 	$v0, 10     
+	j	BinaryPrompt	# Repeat program
+	li 	$v0, 10			# Exit the program (implement exit later)
 	syscall
 
